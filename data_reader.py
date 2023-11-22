4a396512aa503908cf5e27a4df32b14d9e777b05
Luke Lee-Brewin <lukeleebrewin@gmail.com>
2023-11-22 14:55:02 +0000

    fixed time slicing

--- data_reader.py
+++ data_reader.py
@@ -75,17 +75,23 @@ class data_reader:
                     max_time = np.max(time_array)
 
 
-                    # Remove values outside time stamp
-                    time_locs = np.argwhere(np.logical_and(time_array>=t_start, time_array<=t_stop))[0]
-
-
                     # Debugging Messages
                     if verbose:
-                        print(i, len(filt_time_array))
                         print("Start Time: ", datetime.utcfromtimestamp(t_start).strftime('%d%m%Y'), t_start)
                         print("Stop Time: ", datetime.utcfromtimestamp(t_stop).strftime('%d%m%Y'), t_stop)
                         print("Min Time: ", datetime.utcfromtimestamp(min_time).strftime('%d%m%Y'), min_time)
                         print("Max Time: ", datetime.utcfromtimestamp(max_time).strftime('%d%m%Y'), max_time, "\n")
+
+
+
+                    # Remove values outside time stamp
+                    time_locs = np.argwhere(np.logical_and(time_array>=t_start, time_array<=t_stop))
+                    min_time_loc = np.min(time_locs)
+                    max_time_loc = np.max(time_locs)
+
+                    print(f"\nTime Locations Shape: {time_locs.shape}")
+                    print(f"Min Time Index: {min_time_loc}")
+                    print(f"Min Time Index: {max_time_loc}")
 
 
                     # Apply filter to data - mistake probably in this loop
@@ -93,23 +99,20 @@ class data_reader:
                         raw_data = f[detector]['RadiationReading']['spectrum'][()]
                         print(f"Data Found: {raw_data.shape}")
 
-                        filt_data = np.zeros((len(time_locs), raw_data.shape[-1]))
-                        for i, idx in enumerate(time_locs):
-                            filt_data[i, :] = raw_data[idx, :]
+
+                        filt_data = raw_data[min_time_loc:max_time_loc, :]
+                        print(f"Filtered Data Shape: {np.shape(filt_data)}")
+                        data_list.append(filt_data)
+                    
+                    else: print("All Events Filtered")
                             
 
-                        data_list.append(filt_data)
-
-
-
-
-
-
                 except Exception as e:
+                    print(e)
                     continue
-            
-            
-        data_array = np.concatenate(data_list)
+        
+        print(f"Data List Shape: {np.shape(data_list)}")
+        data_array = np.vstack(data_list)
         print(data_array.shape)
         
 
@@ -118,9 +121,9 @@ class data_reader:
 
 if __name__ == "__main__":
     d = data_reader()
-    det_list = d.getDetNames("NaI")
+    det_list = d.getDetNames("NsI")
     
-    d.loadData(det_list[0], t_start="01092018", t_stop="10102018")
+    d.loadData(det_list[0], t_start="01092018", t_stop="02092018")
 
     
 
